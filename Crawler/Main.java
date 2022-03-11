f5a0bbc5f4bd4cefe09a53944dfe63f128839428
Ahmed Lotfy <76037906+AhmedLotfy02@users.noreply.github.com>
2022-03-11 15:03:58 +0200

    crawler/crawler/2/crawler
    first edition of web crawler is added

--- Crawler/Main.java
+++ Crawler/Main.java
@@ -1,10 +1,21 @@
 package webCrawlerw;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<WebCrawler> bots=new ArrayList<>();
+		bots.add(new WebCrawler("www.wikipedia.org",3));
+		for(WebCrawler w:bots) {
+			try {
+				w.getThread().join();
+			}
+			catch(InterruptedException e) {
+			e.printStackTrace();
+			}
+		}
 	}
 
 }
