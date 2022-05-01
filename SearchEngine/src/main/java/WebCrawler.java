0f3433333605d2439a4eea72a5c03c05aa39b068
ahmedasad236 <68563546+ahmedasad236@users.noreply.github.com>
2022-05-01 23:49:10 +0200

    crawler/2/MultithreadedCrawler: Make the crawler uses multiple threads

--- SearchEngine/src/main/java/WebCrawler.java
+++ SearchEngine/src/main/java/WebCrawler.java
@@ -16,8 +16,8 @@ public class WebCrawler implements Runnable {
     private static int pagesCount = 0;
     private Thread thread;
     private String firstLink;
-    private ArrayList<String> visitedLinks = new ArrayList<String>();
-    private ArrayList<String> compactStrings = new ArrayList<>();
+    private static ArrayList<String> visitedLinks = new ArrayList<String>();
+    private static ArrayList<String> compactStrings = new ArrayList<>();
 
     private int ID;
     private URL url;
@@ -30,18 +30,8 @@ public class WebCrawler implements Runnable {
     public WebCrawler(URL url , int num) {
         System.out.println("Web Crawler is created");
         firstLink = url.getHost();
-//        Connection con=Jsoup.connect(url.getHost());
-//        if(con.response().statusCode()!=200){
-//            return; //couldn't connect to the url
-//        }
         System.out.println(firstLink);
         this.url=url;
-
-//        boolean x=CheckRobot(url);
-//        System.out.println(x);
-//        if(!x)
-//            return;
-
         ID = num;
         thread = new Thread(this);
         thread.start();
@@ -60,174 +50,62 @@ public class WebCrawler implements Runnable {
             e.printStackTrace();
         }
     }
-    private void crawlNextPage(String cs , String link) throws IOException {
-        addCS(cs);
-        crawl(link);
-    }
+    //    private void crawlNextPage(String cs , String link) throws IOException {
+//        addCS(cs);
+//        crawl(link);
+//    }
     private void deepCrawl(Element link) throws IOException {
         String nextLink = link.absUrl("href");
-        URL url = new URL(nextLink);
-        String cs = createCS(url);
-        if (!existCS(cs)) {
-            crawlNextPage(cs, nextLink);
-        }
+        crawl(nextLink);
     }
 
     private void extractLinks(Document doc) throws IOException {
-        if (doc == null) {
-            System.out.println("page is null");
-            return;
-        }
-
-        synchronized (this) {
-            pagesCount++;
-            System.out.println(Thread.currentThread().getId() + "page counts: " + pagesCount);
-            System.out.println(compactStrings.size());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-
-                e.printStackTrace();
-            }
-        }
-        for (Element link : doc.select("a[href]")) deepCrawl(link);
+        for (Element link : doc.select("a[href]")) {
+            deepCrawl(link);
+        };
     }
-
+    private void saveDocument(Document doc) throws IOException {
+        String cs = createCS(doc);
+        if(!existCS(cs)) {
+            addCS(cs);
+            extractLinks(doc);
+        }
+    }
     private void crawl(String url) throws IOException {
         if (pagesCount >= MAX_PAGES) return;
         Document doc = request(url);
-        if(doc != null)
-            extractLinks(doc);
+        if(doc != null ) saveDocument(doc);
     }
 
-    public boolean CheckRobot(URL url){
-//        System.out.println(url);
-        String host=url.getHost();
-        String strRobot = "http://" + host + "/robots.txt";
-        URL urlRobot;
-        try { urlRobot = new URL(strRobot);
-        } catch (MalformedURLException e) {
-            // Spam Website
-            return false;
-        }
-        //System.out.println(strRobot);
-        String strCommands;
-        try
-        {
-            InputStream urlRobotStream = urlRobot.openStream();
-            byte b[] = new byte[1000];
-            int numRead = urlRobotStream.read(b);
-//            System.out.println(numRead);
-            if(numRead==-1){
-                return true;
-            }
-            strCommands = new String(b, 0, numRead);
-            while (numRead != -1) {
-                numRead = urlRobotStream.read(b);
-                if (numRead != -1)
-                {
-                    String newCommands = new String(b, 0, numRead);
-                    strCommands += newCommands;
-                }
-            }
-            urlRobotStream.close();
-        }
-        catch (IOException e)
-        {
-            return true; // if there is no robots.txt file, it is OK to search
-        }
-        //System.out.println(strCommands);
-        if (strCommands.contains("Disallow")) // if there are no "disallow" values, then they are not blocking anything.
-        {
-            String[] split = strCommands.split("\n");
-            ArrayList<RobotRule> robotRules = new ArrayList<>();
-            String mostRecentUserAgent = null;
-            boolean useGoogleBotOnly=false;
-            for (int i = 0; i < split.length; i++)
-            {
-                String line = split[i].trim();
-                if (line.toLowerCase().startsWith("user-agent"))
-                {
-                    int start = line.indexOf(":") + 1;
-                    int end   = line.length();
-                    mostRecentUserAgent = line.substring(start, end).trim();
-                   if(useGoogleBotOnly){
-                       break;
-                   }
-                   else if(mostRecentUserAgent.equals("Googlebot") ||mostRecentUserAgent.equals("*")) {
-                       useGoogleBotOnly=true;
-                   }
-                }
-                else if (line.startsWith("Disallow")&&useGoogleBotOnly) {
-                    if (mostRecentUserAgent != null) {
-                        RobotRule r = new RobotRule();
-                        r.userAgent = mostRecentUserAgent;
-                        int start = line.indexOf(":") + 1;
-                        int end   = line.length();
-                        r.rule = line.substring(start, end).trim();
-                        robotRules.add(r);
-                    }
-                }
-            }
-//            System.out.println(robotRules.size());
-            for(RobotRule rw:robotRules){
-//                System.out.println(rw.rule);
-//                System.out.println(rw.userAgent);
-//                System.out.println("------------------------------------");
-                String path = url.getPath();
-              //  if (rw.rule == "/") return false;       // allows nothing if /
-                    String recentUrl="http://" + host+rw.rule;
-                    if(recentUrl.equals(rw.rule)){
-                        return false;
-                    }
-                //System.out.println("http://" + host+rw.rule);
-                if (rw.rule.length() <= path.length())
-                {
-                    String pathCompare = path.substring(0, rw.rule.length());
-                    if (pathCompare.equals(rw.rule)) return false;
-                }
-            }
-
-
-
-        }
-
-
-        return true;
-    }
-    private String createCS(URL url) throws IOException {
+    private String createCS(Document doc) throws IOException {
         try {
-            Document doc = Jsoup.connect(url.toString()).get();
+//            System.out.println(url);
+//            Document doc = Jsoup.connect(url.toString()).get();
+//            System.out.println(url);
             Elements elements = doc.body().select("*");
             String cs = computeCompactString(elements);
             return cs;
         }
-        catch (SocketTimeoutException e) {
-            System.out.println("soso 7elwa");
-            return "";
+        catch(Exception e)
+        {
+            System.out.println("exception");
+            System.out.println(url);
+            return null;
         }
     }
     private boolean existCS(String cs)
     {
         return compactStrings.contains(cs);
     }
-    private void addCS(String cs)
-    {
+    private void addCS(String cs) {
+        synchronized (this) {
+            pagesCount++;
+            System.out.println(Thread.currentThread().getId() + "page counts: " + pagesCount);
+            System.out.println(compactStrings.size());
+        }
         compactStrings.add(cs);
     }
 
-
-    public boolean CompactString(URL url) throws IOException {
-
-        Document doc = Jsoup.connect(url.toString()).get();
-        Elements elements = doc.body().select("*");
-        String cs = computeCompactString(elements);
-        if(compactStrings.contains(cs)) return false;
-        else {
-            compactStrings.add(cs);
-            return true;
-        }
-    }
 
     private String computeCompactString(Elements elements) {
         Integer numOfElements = elements.size();
@@ -252,7 +130,7 @@ public class WebCrawler implements Runnable {
 
         try {
             url = url.startsWith("http") ? url : "https://" + url;
-            Connection con = Jsoup.connect( url);
+            Connection con = Jsoup.connect(url);
             Document doc = con.get();
             if (con.response().statusCode() != 200)
                 System.out.println("Error");
@@ -269,3 +147,98 @@ public class WebCrawler implements Runnable {
         return thread;
     }
 }
+//    public boolean CheckRobot(URL url){
+////        System.out.println(url);
+//        String host=url.getHost();
+//        String strRobot = "http://" + host + "/robots.txt";
+//        URL urlRobot;
+//        try { urlRobot = new URL(strRobot);
+//        } catch (MalformedURLException e) {
+//            // Spam Website
+//            return false;
+//        }
+//        //System.out.println(strRobot);
+//        String strCommands;
+//        try
+//        {
+//            InputStream urlRobotStream = urlRobot.openStream();
+//            byte b[] = new byte[1000];
+//            int numRead = urlRobotStream.read(b);
+////            System.out.println(numRead);
+//            if(numRead==-1){
+//                return true;
+//            }
+//            strCommands = new String(b, 0, numRead);
+//            while (numRead != -1) {
+//                numRead = urlRobotStream.read(b);
+//                if (numRead != -1)
+//                {
+//                    String newCommands = new String(b, 0, numRead);
+//                    strCommands += newCommands;
+//                }
+//            }
+//            urlRobotStream.close();
+//        }
+//        catch (IOException e)
+//        {
+//            return true; // if there is no robots.txt file, it is OK to search
+//        }
+//        //System.out.println(strCommands);
+//        if (strCommands.contains("Disallow")) // if there are no "disallow" values, then they are not blocking anything.
+//        {
+//            String[] split = strCommands.split("\n");
+//            ArrayList<RobotRule> robotRules = new ArrayList<>();
+//            String mostRecentUserAgent = null;
+//            boolean useGoogleBotOnly=false;
+//            for (int i = 0; i < split.length; i++)
+//            {
+//                String line = split[i].trim();
+//                if (line.toLowerCase().startsWith("user-agent"))
+//                {
+//                    int start = line.indexOf(":") + 1;
+//                    int end   = line.length();
+//                    mostRecentUserAgent = line.substring(start, end).trim();
+//                   if(useGoogleBotOnly){
+//                       break;
+//                   }
+//                   else if(mostRecentUserAgent.equals("Googlebot") ||mostRecentUserAgent.equals("*")) {
+//                       useGoogleBotOnly=true;
+//                   }
+//                }
+//                else if (line.startsWith("Disallow")&&useGoogleBotOnly) {
+//                    if (mostRecentUserAgent != null) {
+//                        RobotRule r = new RobotRule();
+//                        r.userAgent = mostRecentUserAgent;
+//                        int start = line.indexOf(":") + 1;
+//                        int end   = line.length();
+//                        r.rule = line.substring(start, end).trim();
+//                        robotRules.add(r);
+//                    }
+//                }
+//            }
+////            System.out.println(robotRules.size());
+//            for(RobotRule rw:robotRules){
+////                System.out.println(rw.rule);
+////                System.out.println(rw.userAgent);
+////                System.out.println("------------------------------------");
+//                String path = url.getPath();
+//              //  if (rw.rule == "/") return false;       // allows nothing if /
+//                    String recentUrl="http://" + host+rw.rule;
+//                    if(recentUrl.equals(rw.rule)){
+//                        return false;
+//                    }
+//                //System.out.println("http://" + host+rw.rule);
+//                if (rw.rule.length() <= path.length())
+//                {
+//                    String pathCompare = path.substring(0, rw.rule.length());
+//                    if (pathCompare.equals(rw.rule)) return false;
+//                }
+//            }
+//
+//
+//
+//        }
+//
+//
+//        return true;
+//    }
